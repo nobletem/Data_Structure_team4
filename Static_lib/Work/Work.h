2b68b8154f05f5b922820de83aa16adbc506e73a
hanleeso <hanleeso1@korea.ac.kr>
2023-05-15 00:21:55 +0900

    5/15

--- Static_lib/Work/Work.h
+++ Static_lib/Work/Work.h
@@ -3,14 +3,19 @@
 
 #include <iostream>
 #include <string>
+#include "Data_Struct.h"
+
+using namespace std;
 
 class Work {
 private:
 	string* work_n;
-	unsigned int* work_data;
+	Queue work_data;
 	int* priority;
 public:
-	
+	void input_work_n(string* work_n);
+    void input_work_data(Queue& work_data, unsigned int data);
+    void input_priority(int* priority);
 };
 
 #endif WORK
