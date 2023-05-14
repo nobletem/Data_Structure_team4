b1a3c8959d44f64d3e87adc6aeed826bb24e6e49
hanleeso <hanleeso1@korea.ac.kr>
2023-05-14 22:43:45 +0900

    05/14 수정

--- Static_lib/Time_Table/Time_Table.cpp
+++ Static_lib/Time_Table/Time_Table.cpp
@@ -6,25 +6,53 @@
 
 using namespace std;
 
-Time_Table::Time_Table() 
+void Time_Table::input_start_t(unsigned int strat_t = 0)
 {
-	int Start_t, end_t, Unit_t;
-	int row = 0;
-	cin >> Start_t >> end_t >> Unit_t;
+	this->Start_t = strat_t;
+}
 
-	row = ceil((COL * (end_t - Start_t))*(60 / Unit_t));	
-	//cout << row << endl;	
-	for (int i = 0; i < COL; i++) {
-		Table[i] = new int[row];
+void Time_Table::input_end_t(unsigned int end_t)
+{
+	this->End_t = end_t;
+}
+
+void Time_Table::input_unit_t(unsigned int unit_t = 0)
+{
+	this->Unit_t = unit_t;
+}
+
+void Time_Table::Gen_Time_Table()
+{	
+	this->row = ceil((this->End_t - this->Start_t)*(60 / this->Unit_t));
+	if(row < 0){
+		cout << "ROW can not be negative" << endl;
+		return;
 	}
-	
+
+	for (int i = 0; i < COL; i++) {
+		Table[i] = new bool[row];
+	}
+
+	for(int i = 0; i < COL; i++)
+		for(unsigned int j = 0; j < this->row; j++){
+			Table[i][j] = false;
+		}
+}
+
+bool Time_Table::show_table(int i, int j)
+{
+	return Table[i][j];
 }
 
 Time_Table::~Time_Table()
 {
-	
 	for (int i = 0; i < COL; i++) {
 		delete [] Table[i];
 	}
-	
 }
+
+unsigned int Time_Table::get_row()
+{
+	return this->row;
+}
+
--- Static_lib/Time_Table/Time_Table.h
+++ Static_lib/Time_Table/Time_Table.h
@@ -10,19 +10,17 @@ using namespace std;
 
 class Time_Table {
 private:
-	int* Table[COL];
+	bool* Table[COL];
+	unsigned int Start_t, End_t, Unit_t;
+	int row;
 public:
-	Time_Table();
+	void Gen_Time_Table();
 	~Time_Table();
-};
-
-class Work {
-private:
-	string* work_n;
-	string* work_data;
-	int* priority;
-public:
-	
+	void input_start_t(unsigned int strat_t);
+	void input_end_t(unsigned int end_t);
+	void input_unit_t(unsigned int unit_t);
+	bool show_table(int i, int j);
+	unsigned int get_row();
 };
 
 #endif TIME_TABLE
