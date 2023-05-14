883b6677ae050401769fe2caca216c6fff344d54
hanleeso <hanleeso1@korea.ac.kr>
2023-05-14 22:43:52 +0900

    5/14 수정

--- Static_lib/Work/Work.h
+++ Static_lib/Work/Work.h
@@ -1,1 +1,16 @@
+#ifndef WORK
+#define WORK
 
+#include <iostream>
+#include <string>
+
+class Work {
+private:
+	string* work_n;
+	unsigned int* work_data;
+	int* priority;
+public:
+	
+};
+
+#endif WORK
