7816e9ee1631c964010f1f39df974a6443a12001
한이소 <hanleeso1@o365jbe.net>
2023-05-29 15:14:57 +0900

    테스트 및 디버깅

--- Main_Console/main_test.cpp
+++ Main_Console/main_test.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main()
 {
-	/* 
+	/*
 	* Queue Test
 	Queue a;
 	for (int i = 0; i < 10; i++) {
@@ -18,29 +18,38 @@ int main()
 		cout << a.deleteQueue() << endl;
 	}
 	*/
-	/*
-	* Time_Table Test
+	
+	// Time_Table Test
 	Time_Table a;
 	a.input_start_t(9);
-	a.input_end_t(13);
+	a.input_end_t(20);
 	a.input_unit_t(60);
 	a.Gen_Time_Table();
+	a.input_work(0, 9, 11);
+	a.input_work(1, 11, 13);
+	a.input_work(2, 10, 20);
+	a.input_work(3, 11, 18);
 	for (int i = 0; i < COL; i++) {
 		for (unsigned int j = 0; j < a.get_row(); j++) {
 			cout << int(a.show_table(i, j)) << " ";
 		}
 		cout << endl;
 	}
-	*/
+	
+
+	/*
+	// Work Test
 	Work a;
 	string str[10];
 	int p[10];
 	int t;
+	unsigned int s;
+
 	cout << "Input work name 6 " << endl;
-	for (int i = 0; i < 6; i++) { // 일 이름 10개 입력
+	for (int i = 0; i < 3; i++) { // 일 이름 10개 입력
 		cout << ">>  ";
 		getline(cin, str[i]);
-		cout << (bool)a.input_work_n(str[i]) << endl;
+		cout << a.input_work_n(str[i]) << endl;
 		cout << str[i] << endl;
 	}
 	cout << "number of Work : " << a.get_size() << endl;
@@ -51,16 +60,16 @@ int main()
 	}
 	cout << "=========================" << endl;
 
-	for (int i = 0; i < 6; i++) {
+	for (int i = 0; i < 3; i++) {
 		cout << " >> ";
 		cin >> p[i];
-		cout << (bool)a.input_priority(p[i]) << endl;
+		cout << a.input_priority(p[i]) << endl;
 		cout << "input " << p[i] << endl;
 	}
 	for (int i = 0; i < a.get_size(); i++) {
 		cout << a.get_priority() << endl;
 	}
-	while(1)
+	while (1)
 	{
 		int index;
 		int temp[5];
@@ -72,12 +81,51 @@ int main()
 
 		cout << "Input D s_h s_m e_h e_m " << endl;
 		for (int i = 0; i < 5; i++) cin >> temp[i];
-		cout << "Testing result : " <<(bool)a.input_work_data(index, temp[0], temp[1], temp[2], temp[3], temp[4]) << endl;
+		cout << "Testing result : " << a.input_work_data(index, temp[0], temp[1], temp[2], temp[3], temp[4]) << endl;
 	}
 	for (int i = 0; i < 5; i++) {
 		t = a.work_data_size(i);
+		s = a.get_work_data(i);
 		for (int j = 0; j < t; j++) {
-			a.work_data_divide(a.get_work_data(i));
+			cout << "Day : " << a.get_day(s) << "s_h : " << a.get_start_t(s) << " e_h : " << a.get_end_t(s) << endl;
 		}
 	}
+	*/
+
+	/*
+	* priority queue test
+	unsigned int data[10];
+
+	for (int i = 0; i < 10; i++) {
+		data[i] = rand() % 100;     // random data
+	}
+
+	cout << "origin data : ";
+	for (int i = 0; i < 10; i++) cout << data[i] << " ";
+	cout << endl;
+
+	heapSort(data, sizeof(data) / sizeof(int));
+	cout << "sorted data : ";
+	for (int i = 0; i < 10; i++) cout << data[i] << " ";
+	cout << endl;
+
+	return 0;
+	*/
+
+	/*
+	// SortandSelect test
+SortandSelect a;
+	int data[10];
+
+	for (int i = 0; i < 10; i++) {
+		data[i] = rand() % 100;     // random data
+	}cout << "origin data : ";
+	for (int i = 0; i < 10; i++) cout << data[i] << " ";
+	cout << endl;
+
+	a.quickSort(data, 0, 9);
+	cout << "sorted data : ";
+	for (int i = 0; i < 10; i++) cout << data[i] << " ";
+	cout << endl;
+	*/
 }
