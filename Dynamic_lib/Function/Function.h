e32b8f05374cfb26b0efb4ca81b8199c96294a7b
suuyyeon <127702506+suuyyeon@users.noreply.github.com>
2023-06-11 23:50:11 +0900

    Update Function.h

--- Dynamic_lib/Function/Function.h
+++ Dynamic_lib/Function/Function.h
@@ -9,5 +9,18 @@ public:
 	bool basic_fuction(Work a);
 };
 
+//function4
+class function4
+{
+private:
+	int workspaceN; 
+	vector<vector<pair<int, int>>> dist_list;
+public:
+	function4(int workspaceN);
+	bool inputDistance();
+	bool addlist(int u, int v, int w);
+	//bool dijkstra_algo(int start, int workspaceN);
+	bool dijkstraAlgo(int start);
+};
 #endif
 
